52bd279580efa6c8a7f7b5bf9c76e32ac53f4a85
kader081 <belghoul081@gmail.com>
2026-05-24 11:23:17 +0100

    + folder of constant/view & file app_color(C-AppColor)

---
+++ code/App_Client/lib/constant/app_color.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  /// color of back ground
+  static const Color backgroundcolor = Color(0xFFFDE8CA);
+  static const Color botounOrange = Color(0xFFFFA11F);
+  static const Color appbarecolor = Color(0xFFFFF0DB);
+  static const Color spaceofwrite_info_massege = Color(0xFFFFD79E);
+  static const Color chate_massege = Color(0xFFFFCC85);
+
+  /// color of text
+  static const Color textbrown = Color(0xFF472900);
+  static const Color textblack = Color(0xFF000000);
+
+  /// standar color 
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color red = Color(0xFFFF0000);
+}
