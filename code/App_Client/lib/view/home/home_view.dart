d22ea8f13faf2aad43fc28130172d955b8db5af9
kader081 <belghoul081@gmail.com>
2026-06-06 03:04:10 +0100

    add bar of locatio

--- code/App_Client/lib/view/home/home_view.dart
+++ code/App_Client/lib/view/home/home_view.dart
@@ -1,7 +1,9 @@
 import 'package:app_pizza_client/view/home/b_N_Bar_home.dart';
 import 'package:app_pizza_client/view/home/categories_home.dart';
 import 'package:app_pizza_client/models/categorymodel/category_Model.dart';
+import 'package:app_pizza_client/view/home/lo_noti_acc.dart';
 import 'package:flutter/material.dart';
+import 'package:app_pizza_client/constant/app_size.dart';
 
 class Home_Page extends StatefulWidget {
   const Home_Page({super.key});
@@ -14,8 +16,10 @@ class Home_Page extends StatefulWidget {
 class _Home_PageState extends State<Home_Page> {
   int _currentIndex = 0;
   int _selectedCategoryIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    double screenWidht = MediaQuery.of(context).size.width;
     return Scaffold(
       bottomNavigationBar: bottomNavigationBar_home(context, _currentIndex, (
         value,
@@ -33,22 +37,31 @@ class _Home_PageState extends State<Home_Page> {
       body: SafeArea(
         child: Column(
           children: [
-            ListView(
-              children: [
-                SizedBox(height: 100),
-                categories(
-                  context: context,
-                  selectedIndex: _selectedCategoryIndex,
-                  onCategorySelected: (index) {
-                    setState(() {
-                      _selectedCategoryIndex = index;
-                    });
-                    print(
-                      "تم اختيار فئة: ${Category_Data.categories[index].name}",
-                    );
-                  },
-                ),
-              ],
+            Container(
+              width: screenWidht,
+              child: Image.asset(
+                "assets/images/home_images/logo_home.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Bar_Location_Notificaion_Bccount( context: context),
+                  categories(
+                    context: context,
+                    selectedIndex: _selectedCategoryIndex,
+                    onCategorySelected: (index) {
+                      setState(() {
+                        _selectedCategoryIndex = index;
+                      });
+                      print(
+                        "تم اختيار فئة: ${Category_Data.categories[index].name}",
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
---
+++ code/App_Client/lib/view/home/lo_noti_acc.dart
@@ -0,0 +1,81 @@
+import 'package:app_pizza_client/constant/app_color.dart';
+import 'package:app_pizza_client/constant/app_size.dart';
+import 'package:app_pizza_client/models/client/client_Model.dart';
+import 'package:flutter/material.dart';
+
+Widget Bar_Location_Notificaion_Bccount({required BuildContext context}) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: context.heightPct(1)),
+    padding: EdgeInsets.symmetric(horizontal: context.heightPct(2)),
+    height: context.heightPct(8),
+    width: context.widthPct(100),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: context.heightPct(24),
+          child: Row(
+            children: [
+              Icon(
+                Icons.location_on_outlined,
+                color: ColorApp_Botton.bottonOrange,
+                size: context.heightPct(7),
+              ),
+              Expanded(
+                child: Text(
+                  "siamitale , 400",
+                  style: TextStyle(
+                    color: ColorApp_Text.textbrown,
+                    fontSize: context.heightPct(2.5),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: context.heightPct(8),
+          child: MaterialButton(
+            onPressed: () {},
+            child: Icon(
+              Icons.notifications_none_rounded,
+              color: ColorApp_Botton.bottonOrange,
+              size: context.heightPct(7),
+            ),
+          ),
+        ),
+        Container(
+          width: context.heightPct(8),
+          height: context.heightPct(8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            border: Border.all(color: ColorApp_Botton.bottonOrange),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.asset(
+                    "assets/images/profila_pucture.png",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned.fill(
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.all(Radius.circular(19)),
+                      onTap: () {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
